3da4c96c6fbff0d7837cd574fe40d6ed3504a704
Natalia <natalia.gomez03@uptc.edu.co>
2024-05-03 18:17:02 -0500

    Diseño interfaz de la consola para permitir al usuario ingresar la ecuacion

--- src/utils/funcionesaux.py
+++ src/utils/funcionesaux.py
@@ -1,1 +1,23 @@
 """Funciones auxiliares que sirvan en varios lugares del código, como funciones para graficar, validar entradas, etc."""
+class FuncionesAuxiliares:
+
+    def capturar_ecuacion():
+     # Capturar la ecuación ingresada por el usuario
+        print("Estas son las operaciones y respectivos simbolos que puede usar para escribir la ecuacion",
+              "Suma: +"
+              "Resta: -"
+              "Multiplicacion: *"
+              "Division: /"
+              "Potencia: ^ o **"
+              "Raiz cuadrada: sqrt()"
+              "Parentesis para agrupar operaciones: ()"
+              "Algunos ejemplos para que vea la utilizacion de los simbolos:"
+              "     2x + 3 = 7"
+              "     x^2 - 4x + 4 = 0"
+              "     (3 * x + 2) / 5 = 7"
+              "     sqrt(x) = 4"
+
+              )
+        ecuacion_str = input("Ingrese la ecuación: ")
+        print (ecuacion_str)
+        
