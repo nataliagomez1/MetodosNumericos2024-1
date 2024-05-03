37aa297986352602f7b497f5c3f707744cbc4093
Jazmin <jazmin.moreno01@uptc.edu.co>
2024-05-03 17:10:23 -0500

    logica de biseccion

--- src/methods/biseccion.py
+++ src/methods/biseccion.py
@@ -1,1 +1,35 @@
-"""Logica metodo BISECCION"""
+def bisection_method(function, left, right, tol, max_iter):
+    """
+    Encuentra la raíz de la función f en el intervalo [a, b] usando el método de bisección.
+    
+    Args:
+        function (function): La función de la cual encontrar la raíz.
+        left (float): Extremo izquierdo del intervalo.
+        right (float): Extremo derecho del intervalo.
+        tol (float): Tolerancia: criterio de convergencia (precisión deseada).
+        max_iter (int): Número máximo de iteraciones permitidas.
+        
+    Returns:
+        float: La aproximación de la raíz de f.
+    """
+    if function(left) * function(right) >= 0:
+        return None
+    
+    iter_count = 0
+    while (right - left) / 2 > tol:
+        medium = (left + right) / 2
+        
+        if function(medium) == 0:
+            return medium
+        
+        if function(medium) * function(left) < 0:
+            right = medium  
+        else:
+            left = medium  
+        
+        iter_count += 1
+        
+        if iter_count > max_iter:
+            return None
+    
+    return (left + right) / 2
--- src/utils/funcionesaux.py
+++ src/utils/funcionesaux.py
@@ -1,7 +1,15 @@
-"""Funciones auxiliares que sirvan en varios lugares del código, como funciones para graficar, validar entradas, etc."""
 import sympy as sp
 
 def validate_function(function):
+    """
+    Valida si la cadena de texto representa una función matemática válida en términos de 'x'.
+
+    Args:
+        function (str): Cadena de texto que representa la función matemática.
+
+    Returns:
+        bool: True si la función es válida, False si no lo es.
+    """
     try:
         x = sp.symbols('x')
         sp.sympify(function.replace("^", "**"))
@@ -10,7 +18,17 @@ def validate_function(function):
         return False
     
 def validate_parameters_puntofijo(starting_point, auxiliary_function):
-    
+    """
+    Valida los parámetros necesarios para el método del punto fijo.
+
+    Args:
+        starting_point (str): Punto inicial para el método del punto fijo.
+        auxiliary_function (str): Función auxiliar utilizada en el método del punto fijo.
+
+    Returns:
+        tuple or None: Una tupla que contiene la función auxiliar validada y el punto inicial convertido a float,
+                       o None si ocurre algún error de validación.
+    """
     if not validate_function(auxiliary_function):
         print("Error: La función auxiliar no es válida.")
         return None
