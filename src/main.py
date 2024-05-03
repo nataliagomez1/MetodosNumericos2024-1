75ca4b16a12edba1e1be6969fe3f8d417ef526d9
DajerlyMoreno <dajerlymoga@gmail.com>
2024-05-03 10:59:10 -0500

    menu principal y parametros punto fijo

--- src/main.py
+++ src/main.py
@@ -1,1 +1,38 @@
-""" interacción con el usuario y la selección de métodos numéricos"""
+from utils.funcionesaux import *
+
+def show_menu():
+    print("1. Metodo Punto Fijo")
+    print("2. Metodo de Biseccion")
+    print("0. Salir")
+
+def choose_method():
+    while True:
+        show_menu()
+        method = input("Ingrese el numero del metodo deseado: ")
+        if method.isdigit():
+            method = int(method)
+            if method == 1:
+                print("\t*** Metodo Punto Fijo ***")
+                
+                while True:
+                    function = input("Ingrese la funcion auxiliar: ")
+                    point = input("Ingrese el punto inicial: ")
+                    
+                    parameters = validate_parameters_puntofijo(point, function)
+                    if parameters is not None:
+                        f , x0 = parameters
+                        #logico del codigo
+                        break
+                
+            elif method == 2:
+                print("\t*** Metodo de Biseccion ***")
+            elif method == 0:
+                print("Saliendo del programa")
+                break
+            else :
+                print("Opción inválida. Intente de nuevo.")
+        else:
+            print("Entrada inválida. Ingrese un número entero.")
+
+if __name__ == "__main__":
+    choose_method()
--- src/utils/funcionesaux.py
+++ src/utils/funcionesaux.py
@@ -1,1 +1,27 @@
 """Funciones auxiliares que sirvan en varios lugares del código, como funciones para graficar, validar entradas, etc."""
+import sympy as sp
+
+def validate_function(function):
+    try:
+        x = sp.symbols('x')
+        sp.sympify(function.replace("^", "**"))
+        return True
+    except (sp.SympifyError, TypeError):
+        return False
+    
+def validate_parameters_puntofijo(starting_point, auxiliary_function):
+    
+    if not validate_function(auxiliary_function):
+        print("Error: La función auxiliar no es válida.")
+        return None
+    
+    x = sp.symbols('x')
+    function = sp.sympify(auxiliary_function.replace("^", "**"))
+    
+    try:
+        starting_point = float(starting_point)
+    except ValueError:
+        print("Error: El punto inicial no es válido.")
+        return None
+    
+    return function, starting_point
