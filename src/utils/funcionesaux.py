b036d43d9789b91f100ef4f1c55bfed8920f2830
Natalia <natalia.gomez03@uptc.edu.co>
2024-05-04 12:28:34 -0500

    Identificacion de parametros para metodo de biseccion y Desarrollo de logica para capturarlos

--- src/utils/funcionesaux.py
+++ src/utils/funcionesaux.py
@@ -1,39 +1,38 @@
 """Funciones auxiliares que sirvan en varios lugares del código, como funciones para graficar, validar entradas, etc."""
 from sympy import symbols, Eq, sqrt
-class FuncionesAuxiliares:
     
-    def capturar_ecuacion():
-        while True:
-            print("Estas son las operaciones y respectivos simbolos que puede usar para escribir la ecuacion",
-                "Suma: +"
-                "Resta: -"
-                "Multiplicacion: *"
-                "Division: /"
-                "Potencia: ^ o **"
-                "Raiz cuadrada: sqrt()"
-                "Parentesis para agrupar operaciones: ()"
-                "Algunos ejemplos para que vea la utilizacion de los simbolos:"
-                "     2x + 3 = 7"
-                "     x^2 - 4x + 4 = 0"
-                "     (3 * x + 2) / 5 = 7"
-                "     sqrt(x) = 4"
+def capturar_ecuacion():
+    while True:
+        print("Estas son las operaciones y respectivos simbolos que puede usar para escribir la ecuacion",
+            "Suma: +"
+            "Resta: -"
+            "Multiplicacion: *"
+            "Division: /"
+            "Potencia: ^ o **"
+            "Raiz cuadrada: sqrt()"
+            "Parentesis para agrupar operaciones: ()"
+            "Algunos ejemplos para que vea la utilizacion de los simbolos:"
+            "     2x + 3 = 7"
+            "     x^2 - 4x + 4 = 0"
+            "     (3 * x + 2) / 5 = 7"
+            "     sqrt(x) = 4"
 
-                )
-            ecuacion_str = input("Ingrese la ecuación")
-            
-            # Verificar si la ecuación contiene caracteres no permitidos
-            if not all(caracter.isdigit() or caracter.isalpha() or caracter.isspace() or caracter in "+-*/^=()" for caracter in ecuacion_str):
-                    print("La ecuación contiene caracteres no permitidos. Por favor, inténtelo nuevamente.")
-                    continue
-            try:
-                ecuacion_str = ecuacion_str.replace("^", "**")  # Reemplazar ^ con ** para potencias
-                x = symbols('x')
-                ecuacion = Eq(eval(ecuacion_str.split('=')[0]), 0)
-                return ecuacion        
-            except Exception as e:
-                print("Error al procesar la ecuación:", e)
-                print("Por favor, inténtelo nuevamente.")
+            )
+        ecuacion_str = input("Ingrese la ecuación")
+        
+        # Verificar si la ecuación contiene caracteres no permitidos
+        if not all(caracter.isdigit() or caracter.isalpha() or caracter.isspace() or caracter in "+-*/^=()" for caracter in ecuacion_str):
+                print("La ecuación contiene caracteres no permitidos. Por favor, inténtelo nuevamente.")
                 continue
+        try:
+            ecuacion_str = ecuacion_str.replace("^", "**")  # Reemplazar ^ con ** para potencias
+            x = symbols('x')
+            ecuacion = Eq(eval(ecuacion_str.split('=')[0]), 0)
+            return ecuacion        
+        except Exception as e:
+            print("Error al procesar la ecuación:", e)
+            print("Por favor, inténtelo nuevamente.")
+            continue
         
 import sympy as sp
 
@@ -80,3 +79,13 @@ def validate_parameters_puntofijo(starting_point, auxiliary_function):
         return None
     
     return function, starting_point
+
+def capturar_parametros_biseccion():        
+    
+    izquierda = float(input("Ingrese el extremo izquierdo del intervalo: "))
+    derecha = float(input("Ingrese el extremo derecho del intervalo: "))
+    
+    tol = float(input("Ingrese la tolerancia (precisión deseada): "))
+    max_iter = int(input("Ingrese el número máximo de iteraciones permitidas: "))
+    
+    return izquierda, derecha, tol, max_iter
