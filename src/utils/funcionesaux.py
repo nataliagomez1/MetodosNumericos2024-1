e2368b7e892b8d5eb52903c84593537574f9faf2
Natalia <natalia.gomez03@uptc.edu.co>
2024-05-03 21:51:55 -0500

    Implementar validaciones para el ingreso de la ecuacion

--- src/utils/funcionesaux.py
+++ src/utils/funcionesaux.py
@@ -1,31 +1,39 @@
 """Funciones auxiliares que sirvan en varios lugares del código, como funciones para graficar, validar entradas, etc."""
+from sympy import symbols, Eq, sqrt
 class FuncionesAuxiliares:
-    from sympy import symbols, Eq, sqrt
-def capturar_ecuacion():
-    print("Estas son las operaciones y respectivos simbolos que puede usar para escribir la ecuacion",
-              "Suma: +"
-              "Resta: -"
-              "Multiplicacion: *"
-              "Division: /"
-              "Potencia: ^ o **"
-              "Raiz cuadrada: sqrt()"
-              "Parentesis para agrupar operaciones: ()"
-              "Algunos ejemplos para que vea la utilizacion de los simbolos:"
-              "     2x + 3 = 7"
-              "     x^2 - 4x + 4 = 0"
-              "     (3 * x + 2) / 5 = 7"
-              "     sqrt(x) = 4"
+    
+    def capturar_ecuacion():
+        while True:
+            print("Estas son las operaciones y respectivos simbolos que puede usar para escribir la ecuacion",
+                "Suma: +"
+                "Resta: -"
+                "Multiplicacion: *"
+                "Division: /"
+                "Potencia: ^ o **"
+                "Raiz cuadrada: sqrt()"
+                "Parentesis para agrupar operaciones: ()"
+                "Algunos ejemplos para que vea la utilizacion de los simbolos:"
+                "     2x + 3 = 7"
+                "     x^2 - 4x + 4 = 0"
+                "     (3 * x + 2) / 5 = 7"
+                "     sqrt(x) = 4"
 
-              )
-    ecuacion_str = input("Ingrese la ecuación")
-    ecuacion_str = ecuacion_str.replace("^", "**")  # Reemplazar ^ con ** para potencias
-
-    # Convertir la cadena de texto en una expresión simbólica
-    x = symbols('x')
-    ecuacion = Eq(eval(ecuacion_str.split('=')[0]), 0)
-
-    return ecuacion        
-
+                )
+            ecuacion_str = input("Ingrese la ecuación")
+            
+            # Verificar si la ecuación contiene caracteres no permitidos
+            if not all(caracter.isdigit() or caracter.isalpha() or caracter.isspace() or caracter in "+-*/^=()" for caracter in ecuacion_str):
+                    print("La ecuación contiene caracteres no permitidos. Por favor, inténtelo nuevamente.")
+                    continue
+            try:
+                ecuacion_str = ecuacion_str.replace("^", "**")  # Reemplazar ^ con ** para potencias
+                x = symbols('x')
+                ecuacion = Eq(eval(ecuacion_str.split('=')[0]), 0)
+                return ecuacion        
+            except Exception as e:
+                print("Error al procesar la ecuación:", e)
+                print("Por favor, inténtelo nuevamente.")
+                continue
         
 import sympy as sp
 
