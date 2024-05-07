6565da5c361de953906f7ee75b5d660ee9722c33
AlissonPaez <alisson.paez@uptc.edu.co>
2024-05-07 17:32:44 -0500

    Cambios para pruebas unitarias

---
+++ src/test/test_pruebas.py
@@ -0,0 +1,28 @@
+"""Implementacion pruebas unitarias"""
+
+import os
+import sys
+
+# Obtiene la ruta del directorio src
+ruta_src = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Agrega la ruta del directorio src al sys.path
+sys.path.append(ruta_src)
+
+# Ahora deberías poder importar el módulo methods
+from methods.puntofijo import fixedpoint
+
+import pytest
+import math
+
+@pytest.mark.parametrize("function, x0, expected_result", [
+    (lambda x: math.sqrt((x + 5)/2), 2, 1.8534870775631227), 
+    (lambda x: 0.5 * math.sin(x) + 1, 0.5, 1.4986612937 ),
+    (lambda x: (math.cos(x))/3, 0.5, 0.3164984459180211),
+    (lambda x: (x**2 - 1)/3, 0.5, -0.3031768798828125),
+    (lambda x: math.sqrt(math.sin(x)), 0.5, 0.8725745971801507),
+    (lambda x: math.pow(10*x + 5, 1/3), 1.5, 3.3823910122241765),
+])
+def test_fixedpoint(function, x0, expected_result):
+    result = fixedpoint(function, x0)
+    assert round(result, 10) == pytest.approx(expected_result, abs=0.000001)
