1069421fa33b3b93d5230869b8001e7e92fb2c74
Natalia <natalia.gomez03@uptc.edu.co>
2024-05-28 15:44:27 -0500

    Main gauss seidel

--- src/main.py
+++ src/main.py
@@ -6,95 +6,122 @@ from methods.newtonraphson import newton_raphson
 from GUI.grafica_newton import graficar_ecuacion
 from methods.secante import secante 
 from GUI.grafica_secante import graficar_ecuacion
+from methods.gaussseidel import gauss_seidel
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def show_menu():
-    print("1. Metodo Punto Fijo")
-    print("2. Metodo de Biseccion")
-    print("3. Metodo de Newton Raphson")
-    print("4. Metodo secante")
-    print("5. Metodo de Jacobi")
-    print("6. Metodo de Gauss-Seidel")
-    print("0. Salir")
-
-def choose_method():
+def main():
+    # Definir la matriz A y el vector b
+    A = np.array([
+        [4, -1, 0, 0],
+        [-1, 4, -1, 0],
+        [0, -1, 4, -1],
+        [0, 0, -1, 4]
+        
+        # [4, 1, 2, 0],
+        # [3, 5, 1, 0],
+        # [1, 1, 3, 1],
+        # [0, 2, 1, 4]
+    ])
+    b = np.array([1, 1, 1, 1])
+    x0 = np.zeros(len(b))
+    tol = 1e-5
+    max_iter = 100
     
-    while True:
-        show_menu()
-        method = input("Ingrese el numero del metodo deseado: ")
-        if method.isdigit():
-            method = int(method)
-            if method == 1:
-                print("\t*** Metodo Punto Fijo ***")
-                
-                #ecuacion= capturar_ecuacion
-                parametros_puntofijo = validate_parameters_puntofijo()
-                if parametros_puntofijo is not None:
-                    function, x0 = parametros_puntofijo
-                    print(fixedpoint(function, x0, toleration=0.001, iteramax=10))
-                
-            elif method == 2:
-                print("\t*** Metodo de Biseccion ***")
-
-                ecuacion_b = capturar_ecuacion_biseccion()
-                parametros_biseccion = capturar_parametros_biseccion()
-                if parametros_biseccion is not None:
-                        izquierda, derecha, tol, max_iter = parametros_biseccion
-                        print(bisection_method(ecuacion_b, izquierda, derecha, tol, max_iter))
-
-            
-            elif method == 3:
-                print("\t*** Metodo de Newton Raphson ***")
-
-                ecuacion = capturar_ecuacion_newton_raphson()
-                parametros_newton_raphson = capturar_parametros_newton_raphson()
-                if parametros_newton_raphson is not None:
-                        derivada, x0 = parametros_newton_raphson
-                        resultado=(newton_raphson(ecuacion, derivada, x0, tolerancia=0.001, max_iter=10 ))
-                        print(f"La raíz es: {resultado}")
-
-                        graficar_ecuacion()
-                break
-            elif method == 4:  
-                
-                print("\t*** Método de la Secante ***")
+    # Llamar al método gauss_seidel
+    solucion = gauss_seidel(A, b, x0, tol, max_iter)
     
-                ecuacion_funcion, ecuacion_sympy = capturar_ecuacion_secante()
-                parametros_secante = capturar_parametros_secante()
-                if parametros_secante is not None:
-                    x0, x1, tol, max_iter = parametros_secante
-                    
-                    resultado_secante = secante(ecuacion_funcion, x0, x1, tol, max_iter)
-                    if resultado_secante is not None:
-                        print(f"La raíz aproximada es: {resultado_secante}")
-                    else:
-                        print("No se encontró una raíz dentro del número máximo de iteraciones permitido.")
-
-                        graficar_ecuacion()
-                break
-
-            elif method == 5: 
-                break
-
-            elif method == 6: 
-
-                print("\t*** Método de Gauss-Seidel ***")
-                A, b, x0, tol, max_iter = capturar_parametros_gauss_seidel()
-                resultado = (gauss_seidel_method(A, b, x0, tol, max_iter))
-                print(f"La solución utilizando el método de Gauss-Seidel es: {resultado}")
-
-                break
-            
-            elif method == 0:
-                print("Saliendo del programa")
-                break
-            else :
-                print("Opción inválida. Intente de nuevo.")
-        else:
-            print("Entrada inválida. Ingrese un número entero.")
+    # Imprimir la solución
+    print("Solución aproximada:", solucion)
 
 if __name__ == "__main__":
-    choose_method()
+    main()
+
+# def show_menu():
+#     print("1. Metodo Punto Fijo")
+#     print("2. Metodo de Biseccion")
+#     print("3. Metodo de Newton Raphson")
+#     print("4. Metodo secante")
+#     print("5. Metodo de Jacobi")
+#     print("6. Metodo de Gauss-Seidel")
+#     print("0. Salir")
+
+# def choose_method():
+    
+#     while True:
+#         show_menu()
+#         method = input("Ingrese el numero del metodo deseado: ")
+#         if method.isdigit():
+#             method = int(method)
+#             if method == 1:
+#                 print("\t*** Metodo Punto Fijo ***")
+                
+#                 #ecuacion= capturar_ecuacion
+#                 parametros_puntofijo = validate_parameters_puntofijo()
+#                 if parametros_puntofijo is not None:
+#                     function, x0 = parametros_puntofijo
+#                     print(fixedpoint(function, x0, toleration=0.001, iteramax=10))
+                
+#             elif method == 2:
+#                 print("\t*** Metodo de Biseccion ***")
+
+#                 ecuacion_b = capturar_ecuacion_biseccion()
+#                 parametros_biseccion = capturar_parametros_biseccion()
+#                 if parametros_biseccion is not None:
+#                         izquierda, derecha, tol, max_iter = parametros_biseccion
+#                         print(bisection_method(ecuacion_b, izquierda, derecha, tol, max_iter))
+
+            
+#             elif method == 3:
+#                 print("\t*** Metodo de Newton Raphson ***")
+
+#                 ecuacion = capturar_ecuacion_newton_raphson()
+#                 parametros_newton_raphson = capturar_parametros_newton_raphson()
+#                 if parametros_newton_raphson is not None:
+#                         derivada, x0 = parametros_newton_raphson
+#                         resultado=(newton_raphson(ecuacion, derivada, x0, tolerancia=0.001, max_iter=10 ))
+#                         print(f"La raíz es: {resultado}")
+
+#                         graficar_ecuacion()
+#                 break
+#             elif method == 4:  
+                
+#                 print("\t*** Método de la Secante ***")
+    
+#                 ecuacion_funcion, ecuacion_sympy = capturar_ecuacion_secante()
+#                 parametros_secante = capturar_parametros_secante()
+#                 if parametros_secante is not None:
+#                     x0, x1, tol, max_iter = parametros_secante
+                    
+#                     resultado_secante = secante(ecuacion_funcion, x0, x1, tol, max_iter)
+#                     if resultado_secante is not None:
+#                         print(f"La raíz aproximada es: {resultado_secante}")
+#                     else:
+#                         print("No se encontró una raíz dentro del número máximo de iteraciones permitido.")
+
+#                         graficar_ecuacion()
+#                 break
+
+#             elif method == 5: 
+#                 break
+
+#             elif method == 6: 
+
+#                 print("\t*** Método de Gauss-Seidel ***")
+#                 A, b, x0, tol, max_iter = capturar_parametros_gauss_seidel()
+#                 resultado = (gauss_seidel(A, b, x0, tol, max_iter))
+#                 print(f"La solución utilizando el método de Gauss-Seidel es: {resultado}")
+
+#                 break
+            
+#             elif method == 0:
+#                 print("Saliendo del programa")
+#                 break
+#             else :
+#                 print("Opción inválida. Intente de nuevo.")
+#         else:
+#             print("Entrada inválida. Ingrese un número entero.")
+
+# if __name__ == "__main__":
+#     choose_method()
